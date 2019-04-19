9e7fe60390719509855faea775460f5bfeb4c99b
Jérémy Somsouk <jh.somsouk@gmail.com>
2019-04-19 19:47:55 +0200

    First commit

---
+++ src/main/java/com/soukiyaki/GameON/GameOnApplication.java
@@ -0,0 +1,13 @@
+package com.soukiyaki.GameON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOnApplication.class, args);
+	}
+
+}
